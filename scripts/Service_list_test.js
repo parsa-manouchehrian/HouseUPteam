43d9d16458849d32d972a735cd0684c3a92f7f7d
Justin Majam <pmajam@my.bcit.ca>
2019-11-27 20:12:32 -0800

    trying to print the services

--- scripts/Service_list_test.js
+++ scripts/Service_list_test.js
@@ -1,1 +1,10 @@
-var services = db.collection
+
+let services = db.collection("Contractors").doc("JPPQs9Amk90ZW7rfQ7sn");
+
+services.get().then(function (doc) {
+    if (doc.exists) {
+        console.log(doc.data().Services);
+    } else {
+        console.log("No DATA")
+    }
+})
